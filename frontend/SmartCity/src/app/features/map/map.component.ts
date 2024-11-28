91c16f6bbc2625f1cc4cef027dcce87598848cff
vetall7 <shapovalovvit228@gmail.com>
2024-11-28 13:43:11 +0100

    feat: add map

--- frontend/SmartCity/src/app/features/map/map.component.ts
+++ frontend/SmartCity/src/app/features/map/map.component.ts
@@ -1,12 +1,82 @@
 import { Component } from '@angular/core';
+import { LeafletModule } from '@asymmetrik/ngx-leaflet';
+import * as Leaflet from 'leaflet';
 
 @Component({
   selector: 'app-map',
   standalone: true,
-  imports: [],
+  imports: [LeafletModule],
   templateUrl: './map.component.html',
   styleUrl: './map.component.scss'
 })
 export class MapComponent {
+  map!: Leaflet.Map;
+  markers: Leaflet.Marker[] = [];
+  options = {
+    layers: [
+      Leaflet.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+      })
+    ],
+    zoom: 12,
+    center: { lat: 28.626137, lng: 79.821603 }
+  }
 
+  private marker = Leaflet.icon({
+    iconUrl: '../../../public/assets/map-marker.svg',
+    shadowUrl: '../../../../public/assets/map-marker.svg',
+
+    iconSize:     [38, 95], // size of the icon
+    shadowSize:   [50, 64], // size of the shadow
+    iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+    shadowAnchor: [4, 62],  // the same for the shadow
+    popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+  });
+
+  initMarkers() {
+    const initialMarkers = [
+      {
+        position: { lat: 28.625485, lng: 79.821091 },
+        draggable: true
+      },
+      {
+        position: { lat: 28.625293, lng: 79.817926 },
+        draggable: false
+      },
+      {
+        position: { lat: 28.625182, lng: 79.81464 },
+        draggable: true
+      }
+    ];
+    for (let index = 0; index < initialMarkers.length; index++) {
+      const data = initialMarkers[index];
+      const marker = this.generateMarker(data, index);
+      marker.addTo(this.map).bindPopup(`<b>${data.position.lat},  ${data.position.lng}</b>`);
+      this.map.panTo(data.position);
+      this.markers.push(marker)
+    }
+  }
+
+  generateMarker(data: any, index: number) {
+    return Leaflet.marker(data.position, { draggable: data.draggable, icon: this.marker })
+      .on('click', (event) => this.markerClicked(event, index))
+      .on('dragend', (event) => this.markerDragEnd(event, index));
+  }
+
+  onMapReady($event: Leaflet.Map) {
+    this.map = $event;
+    this.initMarkers();
+  }
+
+  mapClicked($event: any) {
+    console.log($event.latlng.lat, $event.latlng.lng);
+  }
+
+  markerClicked($event: any, index: number) {
+    console.log($event.latlng.lat, $event.latlng.lng);
+  }
+
+  markerDragEnd($event: any, index: number) {
+    console.log($event.target.getLatLng());
+  } 
 }
