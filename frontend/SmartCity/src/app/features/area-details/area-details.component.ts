819a4e79891db6309b4db60b09c05d55a014e65a
Ruslan Rabadanov <ruslanrabadanov2101@gmail.com>
2024-11-28 12:47:34 +0100

    feat: init project structure

---
+++ frontend/SmartCity/src/app/features/area-details/area-details.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-area-details',
+  standalone: true,
+  imports: [],
+  templateUrl: './area-details.component.html',
+  styleUrl: './area-details.component.scss'
+})
+export class AreaDetailsComponent {
+
+}
