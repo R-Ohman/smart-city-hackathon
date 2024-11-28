ee2e11e05bf42f488909b80860907381ed3b4aa7
vetall7 <shapovalovvit228@gmail.com>
2024-11-28 17:17:32 +0100

    feat: zoom to station

--- frontend/SmartCity/src/app/features/map/map.component.ts
+++ frontend/SmartCity/src/app/features/map/map.component.ts
@@ -51,7 +51,7 @@ export class MapComponent implements OnInit{
     for (let index = 0; index < airStations.length; index++) {
       const data = airStations[index];
       const marker = this.generateMarker(data, index);
-      marker.addTo(this.map).bindPopup(`<b>vitalii</b>`);
+      marker.addTo(this.map);
       this.map.panTo({lat: +data.gegrLat, lng: +data.gegrLon});
       this.markers.push(marker)
     }
@@ -70,10 +70,13 @@ export class MapComponent implements OnInit{
 
   mapClicked($event: any) {
     console.log($event.latlng.lat, $event.latlng.lng);
-    console.log(this.gentNearestStation($event.latlng.lat, $event.latlng.lng));
+    const nearest_station = this.getNearestStation($event.latlng.lat, $event.latlng.lng);
+    this.map.flyTo({lat: +nearest_station.gegrLat, lng: +nearest_station.gegrLon}, 15);
+    this.markers.filter((marker) => marker.getLatLng().lat == +nearest_station.gegrLat 
+    && marker.getLatLng().lng == +nearest_station.gegrLon)[0].bindPopup("vitalii").openPopup();
   }
 
-  gentNearestStation(latitude: number, longtitude: number): AirStation {
+  getNearestStation(latitude: number, longtitude: number): AirStation {
     const stations = this.airStations();
 
     const distance = (x0: number, y0: number, x1: number, y1: number) => Math.hypot(x1 - x0, y1 - y0);
