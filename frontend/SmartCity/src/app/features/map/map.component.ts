78086b2ea227bc67a5bd33c20943a3ec3c7dcdb8
Ruslan Rabadanov <ruslanrabadanov2101@gmail.com>
2024-11-29 06:26:53 +0100

    fix: nearest station

--- frontend/SmartCity/src/app/features/map/map.component.ts
+++ frontend/SmartCity/src/app/features/map/map.component.ts
@@ -210,7 +210,7 @@ export class MapComponent implements OnInit{
   }
 
   getNearestStation(latitude: number, longtitude: number): AirStation {
-    const stations = this.airStations();
+    const stations = this.markers.filter((m) => this.map.hasLayer(m)).map((m) => m.data!);
 
     const distance = (x0: number, y0: number, x1: number, y1: number) => Math.hypot(x1 - x0, y1 - y0);
 
